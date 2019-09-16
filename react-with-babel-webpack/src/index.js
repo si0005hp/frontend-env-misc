a145a5df109529c189c4e816fa7928168d03b20d
si0005hp <si0005hp@gmail.com>
2019-09-17 07:45:49 +0900

    react-with-babel-webpack

---
+++ react-with-babel-webpack/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (<h2>This is react with babel and webpack App</h2>)
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
---
+++ react-with-babel-webpack/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      }
+    ]
+  }
+};
